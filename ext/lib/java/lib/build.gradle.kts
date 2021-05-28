16113c34be0897fee2252def3001ce59b25da524
moznion <moznion@mail.moznion.net>
2021-05-28 21:53:05 +0900

    Add missing gRPC dependency for Java
    Signed-off-by: moznion <moznion@mail.moznion.net>

--- ext/lib/java/lib/build.gradle.kts
+++ ext/lib/java/lib/build.gradle.kts
@@ -9,6 +9,7 @@ repositories {
 }
 
 dependencies {
+    implementation("com.google.protobuf:protobuf-java:3.17.1")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
 }
