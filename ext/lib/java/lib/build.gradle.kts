87dfd8a732b0bceaab57b719b91e417bdc1cc8cd
moznion <moznion@mail.moznion.net>
2021-05-28 21:53:55 +0900

    releng: prepare the next version `0.2.2-SNAPSHOT`
    Signed-off-by: moznion <moznion@mail.moznion.net>

--- ext/lib/java/lib/build.gradle.kts
+++ ext/lib/java/lib/build.gradle.kts
@@ -30,7 +30,7 @@ publishing {
         create<MavenPublication>("maven") {
             group = "net.moznion"
             artifactId = "wiregarden"
-            version = "0.2.1"
+            version = "0.2.2-SNAPSHOT"
             from(components["java"])
             pom {
                 name.set("wiregarden")
