c59c75ec30945647e37fdc7066175b6868c755b8
moznion <moznion@mail.moznion.net>
2021-06-02 18:03:03 +0900

    Support npm library
    Signed-off-by: moznion <moznion@mail.moznion.net>

---
+++ ext/lib/javascript/README.md
@@ -0,0 +1,8 @@
+# Wiregarden library for JavaScript
+
+## How to generate the gRPC code
+
+```
+$ npm run build-container
+$ npm run gen
+```
---
+++ ext/lib/javascript/lib/wiregarden.js
@@ -0,0 +1,7 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", { value: true });
+
+exports.DevicesPb = require('./protos/devices_pb');
+exports.PeersPb = require('./protos/peers_pb');
+
